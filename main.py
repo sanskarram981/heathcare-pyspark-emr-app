1fdd02736488abbd1d8945f33941fecd4f38188e
Sanskar <sanskarram992@gmail.com>
2025-02-21 22:23:11 +0530

    generating requirements file & parser correction

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from heathcare_data_processor import process
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="spark-emr-job-s3-bucket-arguments")
     parser.add_argument("--input", required=True, help="input-s3-bucket-url")
-    parser.add_argument("--output", required=True, help="output-s3-directory-url")
+    parser.add_argument("--output", required=True, help="output-s3-bucket-url")
     args = parser.parse_args()
 
     process(args.input, args.output)
